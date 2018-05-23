96973479925f2cdbb3cd5c3bd0ca2d46718c31a6
yimckit <39316029+yimckit@users.noreply.github.com>
2018-05-23 08:14:28 +0800

    Update Main.java
    update the program of part b

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.*;
+import java.text.NumberFormat;
 
 public class Main {
 
@@ -17,13 +18,13 @@ public class Main {
 
         // Reads in files and Tokenises the content into array
         String[][][] docArray = genTxtArray(args);
-        // arrayPrint(docArray);
 
-        // Remove Punctuation, space, new lines and quote marks
-        // Word Frequency
         termFreq(docArray);
+
         phaseMatching(docArray);
-        // Phrase Matching
+
+        // PhaseMatching(docArray);
+        // PhraseMatching;
         // Compare each files
         // Ignore matches in quote marks
         // Rank Result
@@ -36,7 +37,7 @@ public class Main {
         System.out.println(msg);
     }
 
-    public static boolean regex(String text, String pattern) {
+    public static boolean regex(String text, String pattern){
         // String to be scanned to find the pattern.
         String line = text;
         String regexPattern = pattern;
@@ -46,48 +47,47 @@ public class Main {
 
         // Now create matcher object.
         Matcher m = r.matcher(line);
-        if (m.find()) {
+        if (m.find( )) {
             return true;
-        } else {
+        }else {
             return false;
         }
     }
 
-    public static void arrayPrint(String[][][] sArray) {
+    public static void arrayPrint(String[][][] sArray){
         // backup code for the array testing
-        for (int d = 0; d < sArray.length; d++) {
+        for (int d = 0; d < sArray.length; d++){
             for (int s = 0; s < sArray[d].length; s++) {
-                for (String w : sArray[d][s]) {
-                    sysPrint("Doc: " + d + " Line:" + s + " word:" + w + "\n");
+                for (String w: sArray[d][s]){
+                    sysPrint( "Doc: " + d + " Line:" + s + " word:" + w + "\n");
                 }
             }
         }
     }
 
-    public static void checkArgs(String[] args) {
+    public static void checkArgs(String[] args){
         // ref: https://stackoverflow.com/questions/890966/what-is-string-args-parameter-in-main-method-java
         // Check # of Args passed
-        for (int i = 0; i < args.length; i++) {
+        for(int i = 0; i < args.length; i++) {
             sysPrint(args[i] + "\n");
         }
 
         // Check if Doc Path is specified
-        if (args.length == 0) {
+        if (args.length == 0){
             sysPrint("No doc path was given! Shutting down now .......");
             //System.exit(1);
         }
-
-
     }
 
-    public static String[][][] genTxtArray(String[] args) {
-        int nDoc = 0;
+    public static String[][][] genTxtArray( String[] args){
+        int nDoc=0;
         int nLine;
         String[][][] docArray;
 
         // ref: https://stackoverflow.com/questions/5694385/getting-the-filenames-of-all-files-in-a-folder
-        File folder = new File("C://Users/User/Desktop/SCC110/Project3TestFiles/");
-        // File folder = new File(args[0]);
+//        File folder = new File("C://Users/User/Desktop/SCC110/Project3TestFiles/");
+        File folder = new File(args[0]);
+
         File[] listOfFiles = folder.listFiles();
 
         docArray = new String[listOfFiles.length][][];
@@ -108,11 +108,13 @@ public class Main {
                     docArray[nDoc] = new String[streamArray.length][];
 
                     // Operation of each array
-                    for (int n = 0; n < streamArray.length; n++) {
+                    for(int n=0; n < streamArray.length; n++){
                         sysPrint("Line" + n + ": " + streamArray[n] + "\n");
                         sysPrint("mCounter" + nDoc + " sCounter" + nLine + "\n");
                         // Tokenize the words from the each sentence
                         // http://javadevnotes.com/java-string-split-tutorial-and-examples
+                        streamArray[n] = streamArray[n].toLowerCase();
+                        streamArray[n] = streamArray[n].replaceAll("[\\,\\.\\'\"\\n]|^-$","");
                         docArray[nDoc][nLine] = streamArray[n].split(" ");
                         nLine++;
                     }
@@ -126,29 +128,29 @@ public class Main {
         return docArray;
     }
 
-    public static void termFreq(String[][][] docArray) {
+    public static void termFreq(String[][][] docArray){
 
         // 升序比较器
         // ref: https://crane-yuan.github.io/2016/08/15/The-map-of-java-sorted-by-value/
-        Comparator<Map.Entry<String, Integer>> valueComparator = new Comparator<Map.Entry<String, Integer>>() {
+        Comparator<Map.Entry<String, Integer>> valueComparator = new Comparator<Map.Entry<String,Integer>>() {
             @Override
             public int compare(Map.Entry<String, Integer> o1,
                                Map.Entry<String, Integer> o2) {
                 // TODO Auto-generated method stub
-                return o1.getValue() - o2.getValue();
+                return o1.getValue()-o2.getValue();
             }
         };
 
-        for (int d = 0; d < docArray.length; d++) {
+        for (int d=0;d<docArray.length;d++){
             // ref: https://stackoverflow.com/questions/21771566/calculating-frequency-of-each-word-in-a-sentence-in-java
             Map<String, Integer> map = new TreeMap<>(Collections.reverseOrder());
-            for (int s = 0; s < docArray[d].length; s++) {
+            for (int s=0;s<docArray[d].length;s++){
                 for (String w : docArray[d][s]) {
-                    w = w.toLowerCase();
-                    // remove punctuation
-                    w = w.replaceAll("[\\,\\s\\.\\'\"\\n]|^-$", "");
+                    //w = w.toLowerCase();
+                    //Remove punctuation
+                    //w = w.replaceAll("[\\,\\s\\.\\'\"\\n]|^-$","");
 
-                    if (w != " " && w != "\n" && w != "" && w.length() > 0) {
+                    if (w != " " && w != "\n"  && w != "" && w.length() > 0){
                         // change all to lower case for more effective analysis
                         Integer n = map.get(w);
                         n = (n == null) ? 1 : ++n;
@@ -162,66 +164,54 @@ public class Main {
 
             // 排序
             // ref: https://stackoverflow.com/questions/5894818/how-to-sort-arraylistlong-in-java-in-decreasing-order
-            Collections.sort(list, valueComparator);
+            Collections.sort(list,valueComparator);
             Collections.reverse(list);
 
             // 默认情况下，TreeMap对key进行升序排序
-            System.out.println("------------Doc " + d + "--------------------");
+            System.out.println("------------Doc " + d +" Frequency Count --------------------");
             for (Map.Entry<String, Integer> entry : list) {
-                System.out.println(entry.getKey() + ":" + entry.getValue());
+                System.out.println( entry.getKey() + ":" + entry.getValue());
             }
         }
     }
 
-    public static double[] phaseMatching(String line1, String line2){
-
+    public static int phaseCompare(String[] phase1, String[] phase2){
         // ref: http://www.avajava.com/tutorials/lessons/how-do-i-use-numberformat-to-format-a-percent.html
         // % format
         NumberFormat defaultFormat = NumberFormat.getPercentInstance();
         defaultFormat.setMinimumFractionDigits(1);
 
-        double wordCount;
-        double matchWords = 0;
+        int matchWords = 0;
         int matchCounter = 0;
-        double[] result = new double[2];
 
-        line1 = line1.toLowerCase();
-        line1 = line1.replaceAll("[\\,\\.\\'\\n]|^-$","");
-        line2 = line2.toLowerCase();
-        line2 = line2.replaceAll("[\\,\\.\\'\\n]|^-$","");
-
-        String[] phase1 = line1.split(" ");
-        String[] phase2 = line2.split(" ");
-
-        sysPrint("doc1 word count: " + phase1.length + " doc2 word count: " + phase2.length);
-
-        wordCount = phase2.length;
+//        sysPrint("doc1 word count: " + phase1.length + " doc2 word count: " + phase2.length);
 
         for (int w1=0; w1 < phase1.length; w1++){
-            sysPrint("Word count" + w1);
             for(int w2=0; w2 < phase2.length; w2++){
-                if( Objects.equals(phase1[w1].toLowerCase(),phase2[w2].toLowerCase())){
+//                sysPrint("Word count" + w2);
+                if( Objects.equals(phase1[w1],phase2[w2])){
                     // First word matched, check next
                     if(((w1 + 2) < phase1.length) && ((w2 + 2) < phase2.length)){
-                        if(Objects.equals(phase1[(w1+2)].toLowerCase(),phase2[(w2+2)].toLowerCase())){
+                        if(Objects.equals(phase1[(w1+2)],phase2[(w2+2)])){
                             matchCounter++;
-                            sysPrint("word count" + (w1) + " Matched phase1: " + phase1[(w1)] + " phase2: " + phase2[(w2)] + " Matched " + matchWords + " match Counter " + matchCounter);
+//                            sysPrint("word count" + (w1) + " Matched phase1: " + phase1[(w1)] + " phase2: " + phase2[(w2)] + " Matched " + matchWords + " match Counter " + matchCounter);
                             for(int wAdj=1; (wAdj + w1) < phase1.length && (wAdj + w2) < phase2.length; wAdj++){
-                                if( Objects.equals(phase1[(w1+wAdj)].toLowerCase(),phase2[(w2+wAdj)].toLowerCase())){
+                                if( Objects.equals(phase1[(w1+wAdj)],phase2[(w2+wAdj)])){
                                     matchWords++;
                                     matchCounter++;
-                                    sysPrint("word count" + (w1+wAdj) + " Matched phase1: " + phase1[(w1+wAdj)] + " phase2: " + phase2[(w2+wAdj)] + " Matched " + matchWords + " match Counter " + matchCounter);
+//                                    sysPrint("word count" + (w1+wAdj) + " Matched phase1: " + phase1[(w1+wAdj)] + " phase2: " + phase2[(w2+wAdj)] + " Matched " + matchWords + " match Counter " + matchCounter);
 
                                     // End of loop problem
                                     if ((wAdj + w1 + 1 == phase1.length) || (wAdj + w2 + 1 == phase2.length)){
-                                        sysPrint("End of the loop");
+//                                        sysPrint("End of the loop");
                                         matchWords++; // Add back the first match
                                         w1 = w1 + matchCounter - 1; // Reduce 1 due to the loop structure
                                         matchCounter = 0;
                                         break; // Stop the loop
                                     }
+
                                 } else {
-                                    sysPrint("No More Matches");
+//                                    sysPrint("No More Matches");
                                     matchWords++; // Add back the first match
                                     w1 = w1 + matchCounter; // Override the Main loop
                                     matchCounter = 0;
@@ -231,15 +221,67 @@ public class Main {
                         }
                     }
                 }
+
             }
         }
-        sysPrint("Matched word count " + matchWords);
-        sysPrint("Total Doc2 Word Count " + wordCount);
-        sysPrint("Match% " +  defaultFormat.format((matchWords / wordCount)));
 
-        result[0] = matchWords;
-        result[1] = wordCount;
+        return matchWords;
 
-        return result;
+    }
+
+    public static int[] wordCount(String[][][] docArray){
+        // String to be scanned to find the pattern.
+        int[] totalWords = new int [docArray.length];
+
+        for(int doc = 0; doc < docArray.length; doc++){
+            for(int line = 0; line < docArray[doc].length; line++){
+                totalWords[doc] += docArray[doc][line].length;
+            }
+        }
+
+        for(int i = 0; i < totalWords.length; i++){
+            sysPrint("Total word count for Doc" + i + " is " + totalWords[i]);
+        }
+
+        return totalWords;
+
+    }
+
+    public static void phaseMatching(String[][][] docArray){
+        sysPrint("------------ Phase Matching --------------------");
+
+        // ref: http://www.avajava.com/tutorials/lessons/how-do-i-use-numberformat-to-format-a-percent.html
+        // % format
+        NumberFormat defaultFormat = NumberFormat.getPercentInstance();
+        defaultFormat.setMinimumFractionDigits(1);
+        int[][] totalMatch = new int[docArray.length][docArray.length];
+        int[] totalWords = new int[docArray.length];
+
+        for(int doc1=0; doc1<docArray.length; doc1++){
+            for(int doc2=0; doc2<docArray.length; doc2++){
+                for(int line1=0; line1<docArray[doc1].length; line1++){
+                    for(int line2=0; line2<docArray[doc2].length; line2++){
+                        int result = phaseCompare(docArray[doc1][line1], docArray[doc2][line2]);
+                        totalMatch[doc1][doc2] += result;
+                        if(doc1==doc2){
+                            totalWords[doc1] += result;
+                        } else {
+//                            sysPrint("Doc " + doc1 + " compare to Doc " + doc2 + " Matched count " + totalMatch[doc1][doc2] + " Total Words " + totalWords[doc2]);
+                        }
+                    }
+                }
+            }
+        }
+
+        sysPrint("------------ Printing the phase matching summary --------------------");
+        for(int doc1=0; doc1<totalMatch.length;doc1++){
+            for (int doc2=0; doc2<totalMatch.length;doc2++){
+                if (doc1!=doc2){
+                    double matching = ((double)totalMatch[doc1][doc2] / (double)totalWords[doc2]);
+                    sysPrint("Doc " + doc1 + " compare to Doc " + doc2 + " Matched " + totalMatch[doc1][doc2] + " / Total " + totalWords[doc2] + " (" + defaultFormat.format(matching) + ") ");
+                }
+            }
+        }
     }
 }
+
